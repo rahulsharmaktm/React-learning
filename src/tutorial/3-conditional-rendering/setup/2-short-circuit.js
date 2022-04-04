6e61b0fc75646223fe1765f78f5ff5c2f3e803b4
sachinmadhwa <kjhasachin@gmail.com>
2022-04-04 17:41:25 +0545

    video 23 show hide component done

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [error, setError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text || "joh"}</h1>
+      {/* {text && <h1>And</h1>}
+      {!text && <h1>Text not ture</h1>} */}
+      <button className="btn" onClick={() => setError(!error)}>
+        Toggle Error
+      </button>
+      {/* {error && <h1>Error..</h1>} */}
+      {error ? (
+        <h3>there is an error...</h3>
+      ):(
+        <h1>
+          there is no error
+        </h1>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
