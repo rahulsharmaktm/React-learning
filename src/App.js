87afb308e5d85add4c3329645265b5cde55463be
sachinmadhwa <kjhasachin@gmail.com>
2022-05-24 20:01:32 +0545

    useState array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import ErrorExample from './tutorial/4-forms/setup/1-controlled-inputs' ; //add items
-import ErrorExample from './tutorial/5-useRef/setup/1-useRef-basics' ;
+// import ErrorExample from './tutorial/5-useRef/setup/1-useRef-basics' ;
+import ErrorExample from './tutorial/1-useState/setup/3-useState-array';
 function App() {
   return (
     <div className='container'>
